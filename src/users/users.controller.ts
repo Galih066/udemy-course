fda1a9a0e299f3571f87ce296a7d65cf9b407762
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2024-12-13 21:54:45 +0800

    feat(Auth): add sign out function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -37,6 +37,13 @@ export class UsersController {
         return user;
     }
 
+    @Post('/signout')
+    signOut(
+        @Session() session: any
+    ) {
+        session.userId = null;
+    }
+
     @Post('/signin')
     async login(
         @Body() body: CreateUserDto,
