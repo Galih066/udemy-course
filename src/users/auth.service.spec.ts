d3781a05d6e6c2db1dc9e020b1e887cc41dcac40
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2024-12-16 20:48:56 +0800

    test(Auth): add user entity as type

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,11 +1,12 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
+import { UserEntity } from "./entities/user.entity";
 
 it('can create an instance of auth service', async () => {
-    const fakeUserService = {
+    const fakeUserService: Partial<UsersService> = {
         find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({ id: 1, email, password })
+        create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as UserEntity)
     }
 
     const module = await Test.createTestingModule({
