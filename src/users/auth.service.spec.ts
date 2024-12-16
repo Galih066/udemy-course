7b9bfe858aa7acd88ee614aaa0eaaedcc814c50d
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2024-12-16 21:29:36 +0800

    test(Auth): add test to hashing password

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test } from "@nestjs/testing";
+import { BadRequestException } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 import { UserEntity } from "./entities/user.entity";
@@ -27,5 +28,13 @@ describe("AuthService", () => {
 
     it('can create an instance of auth service', async () => {
         expect(service).toBeDefined();
+    })
+
+    it('creates a new user with salted and hashed password', async () => {
+        const user = await service.signUp('mN6iX@example.com', 'asdf')
+        expect(user.password).not.toEqual('asdf')
+        const [salt, hash] = user.password.split('.')
+        expect(salt).toBeDefined()
+        expect(hash).toBeDefined()
     })
 });
